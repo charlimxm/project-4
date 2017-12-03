fc944a2afeb97d79aa57472891bd48f04ef29b03
Charmaine <charlimxm@gmail.com>
2017-12-03 08:29:28 +0800

    set up modules

---
+++ index.js
@@ -0,0 +1,62 @@
+require('dotenv').config({silent: true})
+
+const url = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/fatPocketLH'
+const port = process.env.PORT || 4000
+
+// installing all modules
+const bodyParser = require('body-parser') // for accessing POST request
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override') // for accessing PUT / DELETE
+const mongoose = require('mongoose') // for DB
+const path = require('path') // for Public files
+const passport = require('./config/ppConfig') // to register passport strategies
+const session = require('express-session') // to create session and cookies
+const MongoStore = require('connect-mongo')(session) // to store session into db
+
+// require all model files
+
+// require all my route files
+
+// initiating express
+const app = express()
+
+// VIEW ENGINES aka handlebars setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// MIDDLEWARES
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(function (req, res, next) {
+  console.log('Method: ' + req.method + ' Path: ' + req.url)
+  next()
+})
+
+// setup bodyParser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+// setup methodOverride
+app.use(methodOverride('_method'))
+
+mongoose.Promise = global.Promise
+mongoose.connect(url, {
+  useMongoClient: true
+})
+.then(
+  () => { console.log('db is connected') },
+  (err) => { console.log(err) }
+)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}))
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+})
