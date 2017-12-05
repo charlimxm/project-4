a4c58a26057c70dfb64db0887c3ebf569466771d
noobnur <nurhsans@gmail.com>
2017-12-05 19:28:36 +0800

    added pending.handlebars

--- routes/pending_routes.js
+++ routes/pending_routes.js
@@ -3,9 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send("GIMME A PEARING!!!")
+  res.render('pending')
 })
 
+module.exports = router
 
 
 
