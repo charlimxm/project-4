00d3601db3c2d63956540f1a7e92022589b4cf61
noobnur <nurhsans@gmail.com>
2017-12-07 18:15:25 +0800

    codewar update and profile submit stable

---
+++ routes/codewars_routes.js
@@ -0,0 +1,41 @@
+const User = require('../models/user')
+const express = require('express')
+const router = express.Router()
+
+var rp = require('request-promise')
+
+router.put('/:id', (req, res) => {
+  console.log(req)
+  var languages = {}
+  var options = {
+    method: 'GET',
+    uri: 'https://www.codewars.com/api/v1/users/' + req.user.username,
+    json: true // Automatically parses the JSON string in the response
+  }
+  // res.send('https://www.codewars.com/api/v1/users/' + req.body.slug)
+  rp(options)
+    .then(
+      function(obj) {
+        var arr = []
+        var languages = obj.ranks.languages
+        for (x in languages) {
+          var langs = {
+            lang: x,
+            kyu: languages[x].name.replace(' kyu', '')
+          }
+          arr.push(langs)
+        }
+        console.log(obj.honor)
+        User.findByIdAndUpdate(req.user.id, {
+            honor: obj.honor,
+            leaderboardPosition: obj.leaderboardPosition,
+            languages: arr,
+            overallKyu: obj.ranks.overall.name
+          })
+          .then(() => res.redirect(`/profile/${req.user.username}`))
+          .catch(err => console.log(err))
+      })
+
+})
+
+module.exports = router
--- routes/dashboard_routes.js
+++ routes/dashboard_routes.js
@@ -2,9 +2,11 @@ const User = require('../models/user')
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/', (req, res) => {
-  if (!req.user.location) {
-  res.redirect(`/profile/${req.user.username}`)
+  if (!req.user.location || !req.user.preferredLanguage) {
+    // var errorMsg = "please specify preferred location and language to go to dashboard"
+  res.redirect('/')
 } else {
   User.find({
      "username" : { $ne : req.user.username},
